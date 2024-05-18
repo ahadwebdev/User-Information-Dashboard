3d391bb9a61ce4c583b686aea062d1aa3dab0b1e
ahadwebdev <ahadkhalildev>
2024-05-18 22:36:17 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        beVeitnampro: ["Be Vietnam Pro", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
